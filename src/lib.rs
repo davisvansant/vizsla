8a06e9c60670f4ee3032a4c8f94e47b37e53fd3e
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-24 11:42:37 -0700

    initial structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,37 @@
+pub struct Archive {}
 
+impl Archive {
+    pub fn init() -> Archive {
+        Archive {}
+    }
+
+    pub fn create(self) -> Result<(), std::io::Error> {
+        Ok(())
+    }
+
+    pub fn extract(self) -> Result<(), std::io::Error> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        Archive::init();
+    }
+
+    #[test]
+    fn create() {
+        let test_archive = Archive::init();
+        test_archive.create().unwrap();
+    }
+
+    #[test]
+    fn extract() {
+        let test_archive = Archive::init();
+        test_archive.extract().unwrap();
+    }
+}
