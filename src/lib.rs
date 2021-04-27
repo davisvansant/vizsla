11cccedcbc1a6c070d730fb77c4905f3c7a4dc43
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-04-27 12:20:11 -0700

    add struct field, update init and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,23 @@ use flate2::read::GzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::fs::File;
+use std::path::PathBuf;
 use tar::Archive;
 use tar::Builder;
 
-pub struct Archiver {}
+pub struct Archiver {
+    pub file_name: PathBuf,
+}
 
 impl Archiver {
-    pub fn init() -> Archiver {
-        Archiver {}
+    pub fn init(name: &str) -> Archiver {
+        let mut path_buf = PathBuf::with_capacity(20);
+        path_buf.push(name);
+        path_buf.set_extension("tar.gz");
+
+        Archiver {
+            file_name: path_buf,
+        }
     }
 
     pub fn create(&self) -> Result<(), std::io::Error> {
@@ -37,12 +46,16 @@ mod tests {
 
     #[test]
     fn init() {
-        Archiver::init();
+        let test_archiver = Archiver::init("test_archive");
+        assert_eq!(
+            test_archiver.file_name.as_path(),
+            std::path::Path::new("test_archive.tar.gz"),
+        );
     }
 
     #[test]
     fn create() {
-        let test_archive = Archiver::init();
+        let test_archive = Archiver::init("test_archive");
         test_archive.create().unwrap();
         let test_file = std::fs::File::open("some_name.tar.gz").unwrap();
         let test_file_metadata = test_file.metadata().unwrap();
@@ -52,7 +65,7 @@ mod tests {
 
     #[test]
     fn extract() {
-        let test_archive = Archiver::init();
+        let test_archive = Archiver::init("test_archive");
         test_archive.create().unwrap();
         test_archive.extract().unwrap();
         let test_file = std::fs::File::open("./some_name/os-release").unwrap();
